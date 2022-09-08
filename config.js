675c56ba72f4a0c1aca2f7a975ab186dc721b94c
Andrywa321 <96677569+Andrywa321@users.noreply.github.com>
2022-09-08 09:59:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -29,9 +29,9 @@ global.APIKeys = { // APIKey Here
   'https://hardianto.xyz': 'hardianto',
   'https://leyscoders-api.herokuapp.com': 'dappakntlll'
 }
-global.packname = data.namabot
-global.author = data.owner
-global.wm = data.namabot + ' ' + data.owner
+global.packname ='Andry Za Apchier'
+global.author ='6285894148101
+global.wm = Andry Za Apchier + 'Andry Za Apchier ' + Eɪɢʜᴛʏ-Sɪx Mᴅ
 
 global.multiplier = 100 // The higher, The harder levelup
 
